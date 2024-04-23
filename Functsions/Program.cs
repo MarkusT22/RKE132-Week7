3eb46708e7c1490d78d1ab0418fbb397cd6c6af3
MarkusT22 <markus.tammoja@tktk.ee>
2024-04-24 00:36:52 +0300

    add week7 code

---
+++ Functsions/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out):");
+string userChoice = Console.ReadLine();
+
+if(userChoice == "in") 
+{
+    PrintHello();
+}
+else 
+{
+    PrintGoodBye();
+}
+
+
+
+static void PrintHello() //Function / Method
+{
+    Console.WriteLine("Hello world!");
+}
+
+static void PrintGoodBye() 
+{
+    Console.WriteLine("See you later, aligator.");
+}
